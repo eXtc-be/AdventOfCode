13c8e6f286f76e7358e50451d8f3e6637a65dcf2
Chris <chris@vanextergem.eu>
2023-12-16 17:35:25 +0100

    solutions for day 3

---
+++ 03/aoc_03_A.py
@@ -0,0 +1,99 @@
+# aoc_03_A.py - Day 3: Gear Ratios - part 1
+# find numbers that are adjacent to symbols
+# https://adventofcode.com/2023/day/3
+
+
+# imports
+
+
+data_path = './input.txt'
+
+
+def load_data(path):
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+def get_symbols(data):
+    return set(char for line in data for char in line if not char.isdigit())
+
+
+def _check_adjacent(data, i, j, number):
+    left = j - len(number) - 1 if j - len(number) - 1 > 0 else 0
+    right = j
+    if data[i][left] in symbols or data[i][right] in symbols:
+        return True
+    else:
+        above = data[i - 1] if i > 0 else ''
+        below = data[i + 1] if i < len(data) - 1 else ''
+    for line in above, below:
+        if line:
+            for idx in range(left, right + 1):
+                if line[idx] in symbols:
+                    return True
+
+
+def find_numbers(data):
+    """intermediate function to check if I got all numbers without doubles"""
+    for i, line in enumerate(data):
+        number = ''
+        for j, char in enumerate(line):
+            if char.isdigit():
+                number += char
+            else:
+                if number:
+                    yield int(number)
+                    number = ''
+        if number:  # check if a number was at the end of the line
+            yield int(number)
+            number = ''
+
+
+def find_part_numbers(data):
+    for i, line in enumerate(data):
+        number = ''
+        for j, char in enumerate(line):
+            if char.isdigit():
+                number += char
+            else:
+                if number:
+                    if _check_adjacent(data, i, j, number):
+                        yield int(number)
+                    number = ''
+        if number:  # check if a number was found at the end of the line
+            if _check_adjacent(data, i, j, number):
+                yield int(number)
+            number = ''
+
+
+test_data = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+..58
+..592.....
+......755.
+...$.*....
+.664.598..""".splitlines()
+
+if __name__ == "__main__":
+    data_lines = load_data(data_path)
+    # data_lines = test_data
+    print(data_lines)
+
+    symbols = get_symbols(data_lines)
+    symbols.discard(".")
+    print(symbols)
+
+    # numbers = list(find_numbers(data_lines))
+    # print(numbers)
+    # print(len(numbers))
+    #
+    # print(f'End result: {sum(numbers)}')
+
+    numbers = list(find_part_numbers(data_lines))
+    print(numbers)
+    print(len(numbers))
+
+    print(f'End result: {sum(numbers)}')
---
+++ 03/aoc_03_B.py
@@ -0,0 +1,97 @@
+# aoc_03_B.py - Day 3: Gear Ratios - part 2
+# find star symbols that are adjacent to exactly 2 numbers
+# https://adventofcode.com/2023/day/3
+
+
+from aoc_03_A import (
+    data_path,
+    load_data,
+    test_data,
+)
+
+
+def _get_left(line, j):
+    while j >= 0 and line[j].isdigit():
+        j -= 1
+    return j + 1
+
+
+def _get_right(line, j):
+    while j <= len(line)-1 and line[j].isdigit():
+        j += 1
+    return j
+
+
+def _get_number(data, i, j):
+    left = _get_left(data[i], j)
+    right = _get_right(data[i], j)
+    return int(data[i][left:right])
+
+
+def _get_ratio_numbers(data, i, j):
+    coords = []
+    numbers = []
+
+    # first find all adjacent cells with a digit in them
+    if j > 0 and data[i][j-1].isdigit():  # check if cell to the left has a number in it
+        coords.append((i, j-1))
+    if j+1 < len(data[i]) - 1 and data[i][j+1].isdigit():  # check if cell to the right has a number in it
+        coords.append((i, j+1))
+    if i > 0:
+        if data[i-1][j].isdigit():  # check if cell above has a number in it
+            coords.append((i-1, j))
+        else:
+            if j > 0 and data[i-1][j-1].isdigit():  # check if cell above and to the left has a number in it
+                coords.append((i-1, j-1))
+            if j+1 < len(data[i-1]) - 1 and data[i-1][j+1].isdigit():  # check if cell above and to the right has a number in it
+                coords.append((i-1, j+1))
+    if i < len(data) - 1:
+        if data[i+1][j].isdigit():  # check if cell below has a number in it
+            coords.append((i+1, j))
+        else:
+            if j > 0 and data[i+1][j-1].isdigit():  # check if cell below and to the left has a number in it
+                coords.append((i+1, j-1))
+            if j+1 < len(data[i+1]) - 1 and data[i+1][j+1].isdigit():  # check if cell below and to the right has a number in it
+                coords.append((i+1, j+1))
+
+    # if exactly 2 adjacent cells have a digit in them, we can look for the full numbers
+    if len(coords) == 2:
+        for i, j in coords:
+            numbers.append(_get_number(data, i, j))
+        return numbers
+    # if more or less adjacent cell have a digit in them, just return a value that has no impact on the result
+    else:
+        return [0, 0]
+
+
+def find_gears(data):
+    for i, line in enumerate(data):
+        if '*' in line:
+            for j in [p for p, char in enumerate(line) if char == '*']:
+                yield _get_ratio_numbers(data, i, j)
+
+
+def calc_ratios(ratio_numbers):
+    return [num1 * num2 for num1, num2 in ratio_numbers]
+
+
+if __name__ == "__main__":
+    data_lines = load_data(data_path)
+    # data_lines = test_data
+    print(data_lines)
+
+    # print(_get_number(data_lines, 0, 2))  # 467
+    # print(_get_number(data_lines, 0, 6))  # 114
+
+    stars = sum(line.count('*') for line in data_lines)
+    print(stars)
+
+    ratio_numbers = list(find_gears(data_lines))
+    print(ratio_numbers)
+    print(len([num for num in ratio_numbers if num != [0, 0]]))
+
+    ratios = calc_ratios(ratio_numbers)
+    print(ratios)
+    print(len([ra for ra in ratios if ra != 0]))
+
+    print(f'End result: {sum(ratios)}')
