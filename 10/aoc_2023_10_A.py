f0c76a559a0d3b38d61d1b80929a71ffe9a90cac
Chris <chris@vanextergem.eu>
2023-12-23 17:45:26 +0100

    added some more test data

--- 10/aoc_2023_10_A.py
+++ 10/aoc_2023_10_A.py
@@ -10,14 +10,14 @@ DATA_PATH = './input_2023_10'
 
 START = 'S'
 
-DIRECTIONS = {
+DIRECTIONS: dict[str, tuple[int, int]] = {
     'U': (-1, 0),
     'D': (1, 0),
     'L': (0, -1),
     'R': (0, 1),
 }
 
-PIPES = {
+PIPES: dict[str, list[tuple[int, int]]] = {
     'S': [DIRECTIONS['U'], DIRECTIONS['D'], DIRECTIONS['L'], DIRECTIONS['R'], ],
     '|': [DIRECTIONS['U'], DIRECTIONS['D'], ],
     '-': [DIRECTIONS['L'], DIRECTIONS['R'], ],
@@ -29,28 +29,33 @@ PIPES = {
 }
 
 
-def load_data(path):
+def load_data(path: str) -> list[str]:
     with open(path) as f:
         return f.read().splitlines()
 
 
-def create_grid(data_lines):
+def create_grid(data_lines: list[str]) -> list[list[str]]:
     return [[char for char in row] for row in data_lines]
 
 
-def find_start(grid):
+def find_start(grid: list[list[str]]) -> tuple[int, int]:
     for r, row in enumerate(grid):
         for c, col in enumerate(row):
             if col == START:
                 return r, c
 
 
-def _find_neighbour(grid, pipe, row, col, excluded_direction):
+def _find_neighbour(grid: list[list[str]],
+                    pipe: str,
+                    row: int,
+                    col: int,
+                    excluded_direction: tuple[int, ...] = (0, 0)
+                    ) -> tuple[int, int]:
     for direction in PIPES[pipe]:
         if direction == excluded_direction:
-            continue  # skip excluded direction
-        if 0 <= row + direction[0] <= len(grid):  # check vertical bounds
-            if 0 <= col + direction[1] <= len(grid[row + direction[0]]):  # check horizontal bounds
+            continue  # skip excluded direction to avoid going back to where we came from (incomplete loop)
+        if 0 <= row + direction[0] < len(grid):  # check vertical bounds
+            if 0 <= col + direction[1] < len(grid[row + direction[0]]):  # check horizontal bounds
                 neighbour = grid[row + direction[0]][col + direction[1]]
                 for i, axis in enumerate(direction):
                     if axis != 0:
@@ -59,7 +64,11 @@ def _find_neighbour(grid, pipe, row, col, excluded_direction):
                                 return row + direction[0], col + direction[1]
 
 
-def find_loop(grid, start_row, start_col):
+def find_loop(
+        grid: list[list[str]],
+        start_row: int,
+        start_col: int
+) -> list[tuple[int, int]]:
     loop = [(start_row, start_col)]
     current_pipe = START
     current_row, current_col = start_row, start_col
@@ -69,7 +78,7 @@ def find_loop(grid, start_row, start_col):
         # print(current_pipe)
         next_pipe = _find_neighbour(grid, current_pipe, current_row, current_col, excluded_direction)
         loop.append(next_pipe)
-        excluded_direction = current_row - next_pipe[0] , current_col - next_pipe[1]
+        excluded_direction = current_row - next_pipe[0], current_col - next_pipe[1]
         current_row, current_col = next_pipe
         current_pipe = grid[current_row][current_col]
         if current_pipe == START:
