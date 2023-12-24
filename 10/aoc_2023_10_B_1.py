0f1549f5c312ac1e9b13f0294197d4ee18a0fe5b
Chris <chris@vanextergem.eu>
2023-12-24 13:59:28 +0100

    my non working attempts for part 2

---
+++ 10/aoc_2023_10_B_1.py
@@ -0,0 +1,358 @@
+# aoc_2023_10_B_1.py - Day 10: Pipe Maze - part 2
+# find the number of tiles that are enclosed by the loop
+# hoping to solve this by scanning top to bottom, left to right
+# remembering whether we're inside or out the loop, after much tweaking,
+# I finally gave up on this strategy, because what worked for case A didn't for case B and vice versa
+# https://adventofcode.com/2023/day/10
+
+
+from aoc_2023_10_A import (
+    DATA_PATH,
+    load_data,
+    # test_data_1,
+    # test_data_2,
+    create_grid,
+    find_start,
+    find_loop,
+    # START,
+    PIPES,
+)
+
+
+INSIDE = '●'
+
+
+def replace_start(grid: list[list[str]], loop: list[tuple[int, int]]) -> None:
+    """replaces the start pipe with the correct symbol based on its neighbours' directions"""
+
+    # calculate neighbours' directions
+    new_dirs = sorted((
+        (loop[1][0] - loop[0][0], loop[1][1] - loop[0][1]),
+        (loop[-2][0] - loop[-1][0], loop[-2][1] - loop[-1][1])
+    ))
+
+    # find a pipe symbol that has the directions to connect its neighbours
+    for pipe, dirs in PIPES.items():
+        if sorted(dirs) == new_dirs:
+            grid[loop[0][0]][loop[0][1]] = pipe
+
+
+def find_enclosed(grid: list[list[str]], loop: list[tuple[int]]) -> list[str]:
+    enclosed = []
+    for r, row in enumerate(grid):  # loop through all rows in the grid top to bottom
+        # inside serves 2 purposes:
+        #   1. remember what character got us inside the loop, and
+        #   2. indicate we're inside the loop if not empty
+        inside = ''  # reset inside at the start of every row
+        for c, char in enumerate(row):  # loop through all columns in the current row left to right
+            if (r, c) in loop:
+                if inside:
+                    if inside == '|':  # enter
+                        if char in '|LF':  # exit
+                            inside = ''
+                        else:  # '-J7'
+                            enclosed.append(char)  # grab inside tube
+                            grid[r][c] = INSIDE  # mark cell as inside
+                    elif inside == 'L':  # enter
+                        if char in 'J7':  # exit
+                            inside = ''
+                        elif char in '-':  # stay inside
+                            continue
+                        # elif char in '7':  # stay inside and change inside character
+                        #     inside = char
+                        else:  # '|LF'
+                            continue  # never happens
+                    elif inside == 'J':  # enter
+                        if char in '|L':  # exit
+                            inside = ''
+                        elif char in 'F':  # stay inside and change inside character
+                            inside = char
+                        else:  # '-J7'
+                            enclosed.append(char)  # grab inside tube
+                            grid[r][c] = INSIDE  # mark cell as inside
+                    elif inside == 'F':  # enter
+                        if char in '7':  # exit
+                            inside = ''
+                        elif char in '-':  # stay inside
+                            continue
+                        elif char in 'J':  # stay inside and change inside character
+                            inside = char
+                        else:  # '|LF'
+                            continue  # never happens
+                    elif inside == '7':  # enter
+                        if char in '|F':  # exit
+                            inside = ''
+                        elif char in 'L':  # stay inside and change inside character
+                            inside = char
+                        else:  # '-J7'
+                            enclosed.append(char)  # grab inside tube
+                            grid[r][c] = INSIDE  # mark cell as inside
+                else:
+                    inside = char
+            else:
+                if inside:
+                    enclosed.append(char)  # grab inside tube
+                    grid[r][c] = INSIDE  # mark cell as inside
+    return enclosed
+
+
+test_data_1 = """.....
+.S-7.
+.|.|.
+.L-J.
+.....
+""".splitlines()
+
+
+test_data_1a = """......
+.S--7.
+.|..|.
+.L--J.
+......
+""".splitlines()
+
+
+test_data_1b = """......
+.S--7.
+.|..|.
+.|..|.
+.L--J.
+......
+""".splitlines()
+
+
+test_data_1c = """........
+.S----7.
+.|....|.
+.|....|.
+.|....|.
+.|....|.
+.L----J.
+........
+""".splitlines()
+
+
+test_data_1d = """.S----7.
+.|....|.
+.|....|.
+.|....|.
+.|....|.
+.L----J.
+........
+""".splitlines()
+
+
+test_data_1e = """........
+.S----7.
+.|....|.
+.|....|.
+.|....|.
+.|....|.
+.L----J.
+""".splitlines()
+
+
+test_data_1f = """.......
+S----7.
+|....|.
+|....|.
+|....|.
+|....|.
+L----J.
+.......
+""".splitlines()
+
+
+test_data_1g = """.......
+.S----7
+.|....|
+.|....|
+.|....|
+.|....|
+.L----J
+.......
+""".splitlines()
+
+
+test_data_1h = """S----7
+|....|
+|....|
+|....|
+|....|
+L----J
+""".splitlines()
+
+
+test_data_1i = """S----7
+|....|
+|.F--J
+|.|...
+|.|...
+L-J...
+""".splitlines()
+
+
+test_data_2 = """..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+""".splitlines()
+
+
+test_data_3 = """...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+""".splitlines()
+
+
+test_data_3b = """.........
+.F-----7.
+.|F--7.|.
+.||..|.|.
+.||..L-J.
+-||......
+.||..F-7.
+.||..|.|.
+.|L--J.|.
+.S-----J.
+.........
+""".splitlines()
+
+
+test_data_4 = """..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+""".splitlines()
+
+
+test_data_4a = """......
+.S--7.
+.|..|.
+.L7FJ.
+..LJ..
+""".splitlines()
+
+
+test_data_4b = """......
+.S--7.
+.|..|.
+.L7FJ.
+..||..
+..LJ..
+""".splitlines()
+
+
+test_data_4c = """.........
+.F-----7.
+.|F--7.|.
+.||..|.|.
+.||..L-J.
+.||..F-7.
+.||..|.|.
+.|L--J.|.
+.S-----J.
+.........
+""".splitlines()
+
+
+test_data_5 = """.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+""".splitlines()
+
+
+test_data_6 = """FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+""".splitlines()
+
+
+if __name__ == "__main__":
+    # data_lines = load_data(DATA_PATH)
+    # data_lines = test_data_1
+    # data_lines = test_data_2
+    data_lines = test_data_3
+    # data_lines = test_data_4
+    # data_lines = test_data_5
+    # data_lines = test_data_6
+    # print(data_lines)
+
+    grid = create_grid(data_lines)
+    for r in grid:
+        print(r)
+    print()
+
+    start_row, start_col = find_start(grid)
+    # print(f'start character "{START}" was found on row {start_row}, column {start_col}')
+
+    # for row, col in (
+    #         (3, 0),  # |
+    #         (4, 0),  # L
+    #         (4, 1),  # J
+    #         (3, 1),  # F
+    #         (3, 2),  # -
+    #         (2, 3),  # L
+    # ):
+    #     grid = create_grid(test_data_2)
+    #
+    #     grid[2][0] = 'F'
+    #     grid[row][col] = 'S'
+    #     start_row, start_col = row, col
+    #
+    #     for r in grid:
+    #         print(r)
+    #     print()
+    #
+    #     loop = find_loop(grid, start_row, start_col)
+    #     # print(loop)
+    #
+    #     replace_start(grid, loop)
+    #     for r in grid:
+    #         print(r)
+    #     print('-' * 100)
+
+    loop = find_loop(grid, start_row, start_col)
+    # print(loop)
+
+    replace_start(grid, loop)
+    for r in grid:
+        print(r)
+    print()
+
+    enclosed = find_enclosed(grid, loop)
+    print(enclosed)
+    print()
+
+    replace_start(grid, loop)
+    for r in grid:
+        print(r)
+    print()
+
+    print(f'End result: {len(enclosed)}')
---
+++ 10/aoc_2023_10_B_2.py
@@ -0,0 +1,227 @@
+# aoc_2023_10_B_1.py - Day 10: Pipe Maze - part 2
+# find the number of tiles that are enclosed by the loop
+# new tactic: for each and every cell check if we can reach one of the sides without touching the loop
+# I got this working, except for grids where you have to 'squeeze' between pipes - I can't figure out how to implement that
+# https://adventofcode.com/2023/day/10
+
+
+from aoc_2023_10_A import (
+    DATA_PATH,
+    load_data,
+    # test_data_1,
+    # test_data_2,
+    create_grid,
+    find_start,
+    find_loop,
+    # START,
+    PIPES,
+    DIRECTIONS,
+)
+
+from aoc_2023_10_B_1 import (
+    replace_start,
+    test_data_1,
+    test_data_1a,
+    test_data_1b,
+    test_data_1c,
+    test_data_1d,
+    test_data_1e,
+    test_data_1f,
+    test_data_1g,
+    test_data_1h,
+    test_data_1i,
+    test_data_2,
+    test_data_3,
+    test_data_3b,
+    test_data_4,
+    test_data_4a,
+    test_data_4b,
+    test_data_4c,
+    test_data_5,
+    test_data_6,
+)
+
+from enum import Enum, auto
+
+
+class Found(Enum):
+    NOTHING = auto()
+    EMPTY = auto()
+    LOOP = auto()
+    EDGE = auto()
+
+
+# INSIDE = '●'
+# INSIDE = '■'
+INSIDE = '█'
+# INSIDE = '░'
+# INSIDE = '▒'
+# INSIDE = '▓'
+
+PIPE_TO_BOX = {
+    'S': 'S',
+    '|': '│',
+    '-': '─',
+    'L': '└',
+    'J': '┘',
+    'F': '┌',
+    '7': '┐',
+    '.': '.',
+    Found.LOOP: INSIDE,
+}
+
+
+def _find_edge(
+        grid: list[list[str]],
+        loop: list[tuple[int, int]],
+        row: int,
+        col: int,
+        visited: list[tuple[int, int]] = []
+):
+    """
+    starting at row, col, tries to get to the edge of the grid
+    without touching the loop (except for squeezing between 2 parallel pipes)
+    returns None, but changes the cells in the grid to Found.EDGE or Found.LOOP
+    """
+
+    # initialize every direction with NOTHING, except the ones that would land us on an already visited cell
+    found: dict[tuple[int, int], Found] = {
+        direction: Found.NOTHING
+        for direction in DIRECTIONS.values()
+        if (row + direction[0], col + direction[1]) not in visited
+    }
+
+    for direction in found:
+        # check if we hit a part of the loop
+        if (row + direction[0], col + direction[1]) in loop:
+            # check if we can 'squeeze' inbetween pipes
+            # determine which of the axes is non-zero and what its value is
+            axis, step = [(i, s) for i, s in enumerate(direction) if direction[i] != 0][0]
+            pipe = grid[row + direction[0]][col + direction[1]]  # get pipe's directions
+            if any(dir[axis] == step for dir in PIPES[pipe]):
+                # at least one of the pipe's directions is the same as the current searching direction
+                # try to find a way out by squeezing between the pipes
+                # this is where I gave up, it's getting too complicated
+                pass
+            else:
+                # none of the pipe's directions are the same as the current searching direction
+                found[direction] = Found.LOOP
+                continue  # don't look any further in this direction
+
+        # check if we reached a vertical edge
+        if not (0 < row + direction[0] < len(grid) - 1):
+            found[direction] = Found.EDGE
+            break
+
+        # check if we reached a horizontal edge
+        if not (0 < col + direction[1] < len(grid[row + direction[0]]) - 1):
+            found[direction] = Found.EDGE
+            break
+
+        # advance a step in this direction and check if we reached the edge yet
+        _find_edge(
+            grid,
+            loop,
+            row + direction[0],
+            col + direction[1],
+            visited + [(row, col)]  # add current cell to list of visited
+        )
+        found[direction] = grid[row + direction[0]][col + direction[1]]  # update direction with next cell's value
+
+    if Found.EDGE in found.values():
+        grid[row][col] = Found.EDGE
+    elif all(f == Found.LOOP == Found.LOOP for d, f in found.items()):
+        grid[row][col] = Found.LOOP
+
+
+def find_enclosed(grid: list[list[str]], loop: list[tuple[int, int]]) -> None:
+    """marks all cells in the grid with a member of the Found enum (except outer edges)"""
+
+    # cells in the first and last rows and columns can only be part of the loop or outside the loop
+    for r, row in enumerate(grid[1:-1], 1):  # loop through all rows in the grid except the first and last
+        for c, char in enumerate(row[1:-1], 1):  # loop through all columns in the current row except the first and last
+            if (r, c) in loop:
+                continue  # skip any pipe that is part of the loop
+            if grid[r][c] == Found.EDGE or grid[r][c] == Found.LOOP:
+                continue  # skip cells that have already been visited
+            _find_edge(grid, loop, r, c)
+
+
+def draw_grid(grid: list[list[str|Found]]) -> None:
+    height = len(grid)
+    row_label_width = len(str(height-1))
+
+    width = len(grid[0])
+    col_label_height = len(str(width-1))
+
+    # print column numbers
+    for line in range(col_label_height):
+        print(' ' * (row_label_width) + ' ', end='')
+        for col in range(width):
+            if line < col_label_height - 1:
+                print(col // 10 ** (col_label_height - (line + 1)), end='')
+            else:
+                print(col % 10, end='')
+        print()
+
+    # print lines
+    for r, row in enumerate(grid):
+        # print the line number
+        print(f'{r:{row_label_width}} ', end='')
+        for c, char in enumerate(row):
+            print(PIPE_TO_BOX[char], end='')
+        print(f' {r:{row_label_width}}')
+
+    # print column numbers
+    for line in range(col_label_height):
+        print(' ' * (row_label_width) + ' ', end='')
+        for col in range(width):
+            if line < col_label_height - 1:
+                print(col // 10 ** (col_label_height - (line + 1)), end='')
+            else:
+                print(col % 10, end='')
+        print()
+
+    print()
+
+
+if __name__ == "__main__":
+    # data_lines = load_data(DATA_PATH)
+    # data_lines = test_data_1
+    # data_lines = test_data_1a
+    # data_lines = test_data_1b
+    # data_lines = test_data_1c
+    # data_lines = test_data_1d
+    # data_lines = test_data_1e
+    # data_lines = test_data_1f
+    # data_lines = test_data_1g
+    # data_lines = test_data_1h
+    data_lines = test_data_1i
+    # data_lines = test_data_2
+    # data_lines = test_data_3
+    # data_lines = test_data_3b
+    # data_lines = test_data_4
+    # data_lines = test_data_4a
+    # data_lines = test_data_4b
+    # data_lines = test_data_4c
+    # data_lines = test_data_5
+    # data_lines = test_data_6
+    # print(data_lines)
+
+    grid = create_grid(data_lines)
+    # draw_grid(grid)
+
+    start_row, start_col = find_start(grid)
+    # print(f'start character "{START}" was found on row {start_row}, column {start_col}')
+
+    loop = find_loop(grid, start_row, start_col)
+    # print(loop)
+
+    replace_start(grid, loop)
+    # draw_grid([['.'] * 100] * 20)
+    draw_grid(grid)
+
+    find_enclosed(grid, loop)
+    draw_grid(grid)
+
+    print(f'End result: {sum(line.count(Found.LOOP) for line in grid)}')
