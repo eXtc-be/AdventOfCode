120cdd3e48eb9156749c85a00fcf4ea9fdf49903
Chris <chris@vanextergem.eu>
2023-12-26 19:11:17 +0100

    renamed day_number and year_number to day_string and year_string

--- create_files.py
+++ create_files.py
@@ -17,21 +17,21 @@ HTML_FILE = '{}_{}.html'
 INPUT_URL = 'https://adventofcode.com/2023/day/{}/input'
 INPUT_FILE = 'input_{}_{}'
 
-day_number = None
-year_number = 2023
+day_string = None
+year_string = 2023
 
 if len(sys.argv) > 1:
-    day_number = sys.argv[1]
+    day_string = sys.argv[1]
 
-if not day_number:
-    day_number = input('Day number? ')
+if not day_string:
+    day_string = input('Day number? ')
 
 # create folder and file names
-day_number = f'{int(day_number):02d}'
-file_A = f'aoc_{str(year_number)}_{day_number}_A.py'
-file_B = f'aoc_{str(year_number)}_{day_number}_B.py'
+day_string = f'{int(day_string):02d}'
+file_A = f'aoc_{str(year_string)}_{day_string}_A.py'
+file_B = f'aoc_{str(year_string)}_{day_string}_B.py'
 
-path = Path(day_number)
+path = Path(day_string)
 
 # check for existing folder and ask permission to delete/overwrite
 write = False
@@ -53,13 +53,13 @@ if write:
         cookie_data = cookie_file.read().strip()  # read session cookie data from file
         session_cookie = cookiejar_from_dict({'session': cookie_data})  # turn it into a cookie jar for use in session
 
-    input_url = INPUT_URL.format(int(day_number))
-    html_url = HTML_URL.format(int(day_number))
+    input_url = INPUT_URL.format(int(day_string))
+    html_url = HTML_URL.format(int(day_string))
 
-    title = ''  # make sure title is accessible outside with block
-    description = ''  # make sure description is accessible outside with block
+    title = ''  # make sure title is accessible outside with-block
+    description = ''  # make sure description is accessible outside with-block
 
-    # download today's description and input file
+    # download today's description and input files
     with requests.Session() as session:  # create session object
         session.cookies.update(session_cookie)  # set session cookie to 'authenticate'
 
@@ -75,10 +75,12 @@ if write:
             art_match = re.search(r'<article.*?>(.*?)</article>', html, flags=re.DOTALL)
             if art_match:
                 article = art_match.group(1)
+
                 # extract title from article
                 title_match = re.match(r'<h2>--- Day \d\d?: (.*?) ---</h2>', article)
                 if title_match:
                     title = title_match.group(1)
+
                 # extract description from article (it's the last paragraph)
                 para_match = re.findall(r'<p>(.*?)</p>', article)
                 if para_match:
@@ -86,7 +88,7 @@ if write:
                     description = re.sub(r'<.+?>', '', description, flags=re.DOTALL)  # remove any html tags
 
             # save html to a file
-            html_file = HTML_FILE.format(year_number, day_number)
+            html_file = HTML_FILE.format(year_string, day_string)
             print(f'creating file {(path / html_file).absolute()}')
             with open(path / html_file, 'w') as out_file:
                 out_file.write(html)
@@ -97,16 +99,15 @@ if write:
         print(f'downloading input file from {input_url}')
         response = session.get(input_url)  # get the data from the remote server
         if response.status_code == 200:
-            input_file = INPUT_FILE.format(year_number, day_number)
+            input_file = INPUT_FILE.format(year_string, day_string)
             print(f'creating file {(path / input_file).absolute()}')
             with open(path / input_file, 'w') as out_file:
                 out_file.write(response.text)
-                # out_file.write(response.content.decode('utf-8'))  # TODO: check if response.text works
         else:
             print(f'something went wrong, remote server returned status {response.status_code}')
             exit()
 
-    # create both files from template and replace certain placeholders
+    # create both files from template and replace placeholders
     for file, template in zip([file_A, file_B], ['template_A.py', 'template_B.py']):
         print(f'creating file {(path / file).absolute()}')
         with open(template, 'r') as in_file, open(path / file, 'w') as out_file:
@@ -120,11 +121,11 @@ if write:
                 if '<url>' in line:
                     line = line.replace('<url>', html_url)
                 if '<year>' in line:
-                    line = line.replace('<year>', str(year_number))
+                    line = line.replace('<year>', str(year_string))
                 if '<day>' in line:
-                    line = line.replace('<day>', str(int(day_number)))
+                    line = line.replace('<day>', str(int(day_string)))
                 if '<0_day>' in line:
-                    line = line.replace('<0_day>', day_number)
+                    line = line.replace('<0_day>', day_string)
                 if '<file_A>' in line:
                     line = line.replace('<file_A>', file_A[:-3])
                 out_file.write(line)
