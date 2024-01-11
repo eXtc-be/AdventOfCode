143fb13952fcdce30075bfe2bf822b746d53e5cf
Chris <chris@vanextergem.eu>
2024-01-11 17:26:33 +0100

    debug part 2

--- 2015/10/aoc_2015_10_B_1.py
+++ 2015/10/aoc_2015_10_B_1.py
@@ -7,8 +7,8 @@ from aoc_2015_10_A_1 import (
     DATA_PATH,
     load_data,
     test_data,
-    REPEATS,
     look_and_say,
+    REPEATS,
 )
 
 from tools import time_it
@@ -24,20 +24,13 @@ NEW_REPEATS = 50
 @time_it
 def main(data_lines: list[str]) -> None:
     result = data_lines[0]
-    # result = 492982
     # print(result)
 
-    for _ in range(NEW_REPEATS - REPEATS + 1):
+    for _ in range(NEW_REPEATS - REPEATS):
         result = look_and_say(result)
         # print(result)
 
     print(f'End result: {len(result):,}')
-
-    # for _ in range(NEW_REPEATS - REPEATS):
-    #     result = calculate_conway(result)
-    #     # print(result)
-    #
-    # print(f'End result: {round(result):,}')
 
 
 if __name__ == "__main__":
@@ -48,9 +41,5 @@ if __name__ == "__main__":
 
     main(data_lines)
     # using input data:
-    #   End result: 1,166,642 - too low - in 2.9 seconds
-    #               6,985,524 - too low - in less than a millisecond
-    #               9,106,170 - too high - in less than a millisecond
-    #               6,985,522 - incorrect - in less than a millisecond
-    #               9,106,167 - incorrect - in less than a millisecond
-    #               6,989,950 in 8.4 seconds
+    #   End result: 6,989,950 in 8.4 seconds
+    #   Finished 'main' in 6.0 seconds
