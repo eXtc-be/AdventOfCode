7449e1d0c075bae975381386309a7d7fbe8e976b
Chris <chris@vanextergem.eu>
2024-04-24 17:55:12 +0200

    added a way to display the program disassembled

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -26,6 +26,18 @@ class Argument:
     mode: Mode
     value: int
 
+    def __str__(self):
+        value = self.value
+
+        if self.mode == Mode.immediate:
+            pass
+        elif self.mode == Mode.positional:
+            value = f'[{value:0{ADDRESS_WIDTH}}]'
+        elif self.mode == Mode.relative:
+            value = f'{{{value:0{ADDRESS_WIDTH}}}}'
+
+        return f'{value:>{NUMBER_WIDTH}}'
+
 
 @dataclass
 class Computer:
@@ -131,6 +143,9 @@ class Computer:
         self.state = State.halted
         return -1
 
+    def dat(self, args: list[Argument]) -> int:
+        return -1
+
     INSTRUCTION_SET = {
         1: {'function': add, 'args': 3},
         2: {'function': mul, 'args': 3},
@@ -144,7 +159,7 @@ class Computer:
         99: {'function': hlt, 'args': 0},
     }
 
-    def run(self, inputs: list[int] = None) -> int | None:
+    def run(self, inputs: list[int] = None, verbose: bool = False, confirm: bool = False) -> int | None:
         if inputs:
             self.inputs.extend(inputs)
 
@@ -167,10 +182,55 @@ class Computer:
                 )
             ]
 
+            # print instruction
+            if verbose:
+                print(f'{self.ip:0{ADDRESS_WIDTH}d}: '
+                      f'{function.__name__.upper()} '
+                      f'{" ".join([f"{str(arg)}={self._get_arg(arg):{NUMBER_WIDTH}}" for arg in args])}',
+                      end=' ')
+
+                if confirm:
+                    input()
+                else:
+                    print()
+
             # execute instruction
             self.ip = function(self, args)
 
         return self.outputs[-1] if self.outputs else None
+
+    def dump(self):
+        """prints the program *as it is at this moment in time*"""
+        ip = 0
+
+        while True:
+            # read next instruction from memory and convert it to zero padded string
+            instruction = f'{self[ip]:05}'
+            opcode = int(instruction[-2:])  # extract opcode
+
+            # get instruction details
+            num_args = 0
+            function = self.dat
+            if opcode in self.INSTRUCTION_SET:
+                instruction_dict = self.INSTRUCTION_SET[opcode]
+                function = instruction_dict['function']
+                num_args = instruction_dict['args']
+            args = []
+            if num_args:
+                args = [
+                    Argument(Mode(mode), val)
+                    for val, mode in zip(
+                        self[ip + 1:ip + 1 + num_args],
+                        [int(val) for val in reversed(instruction[:3])]
+                    )
+                ]
+
+            # print instruction
+            print(f'{ip:0{ADDRESS_WIDTH}d}: {function.__name__.upper()} {" ".join([str(arg) for arg in args])}')
+
+            ip += num_args + 1
+            if ip > len(self.memory) - 1:
+                break
 
 
 if __name__ == "__main__":
