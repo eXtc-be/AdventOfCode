491b17474b4c41b6e800dffca51d6e4178d35c3a
Chris <chris@vanextergem.eu>
2024-04-24 17:54:12 +0200

    added support for relative addressing mode and auto expanding memory; added rbo instruction

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -8,6 +8,11 @@ from enum import Enum, auto
 class Mode(Enum):
     positional = 0
     immediate = 1
+    relative = 2
+
+
+ADDRESS_WIDTH = 4
+NUMBER_WIDTH = 16
 
 
 class State(Enum):
@@ -30,32 +35,61 @@ class Computer:
 
     def __post_init__(self):
         self.ip = 0
-        self._ip_len = len(f'{len(self.memory)}')
         self.outputs = []
         self.state = State.paused
+        self.rel_base = 0
+
+    def _expand_memory(self, amount: int) -> None:
+        self.memory.extend([0 for _ in range(amount)])
+
+    def _check_index(self, index) -> None:
+        if isinstance(index, slice):
+            if index.stop > len(self.memory) - 1:
+                self._expand_memory(index.stop - len(self.memory) + 1)
+        else:
+            if index > len(self.memory) - 1:
+                self._expand_memory(index - len(self.memory) + 1)
+
+    def __getitem__(self, index) -> int | list[int]:
+        self._check_index(index)
+        return self.memory[index]
+
+    def __setitem__(self, index, value):
+        self._check_index(index)
+        self.memory[index] = value
 
     def _get_arg(self, arg: Argument) -> int:
         if arg.mode == Mode.immediate:
             return arg.value
         elif arg.mode == Mode.positional:
-            return self.memory[arg.value]
+            return self[arg.value]
+        elif arg.mode == Mode.relative:
+            return self[self.rel_base + arg.value]
         else:  # unknown mode - should never happen
-            raise ValueError(f'unknown mode: {arg.mode.name}')
+            raise ValueError(f'unsupported mode: {arg.mode.name}')
+
+    def _set_dst(self, arg: Argument, value: int) -> None:
+        if arg.mode == Mode.positional:
+            self[arg.value] = value
+        elif arg.mode == Mode.relative:
+            self[self.rel_base + arg.value] = value
+        else:  # unknown mode - should never happen
+            raise ValueError(f'unsupported mode: {arg.mode.name}')
 
     def add(self, args: list[Argument]) -> int:
-        assert args[-1].mode == Mode.positional
-        self.memory[args[-1].value] = self._get_arg(args[0]) + self._get_arg(args[1])
+        assert args[-1].mode != Mode.immediate
+        self._set_dst(args[-1], self._get_arg(args[0]) + self._get_arg(args[1]))
         return self.ip + len(args) + 1
 
     def mul(self, args: list[Argument]) -> int:
-        assert args[-1].mode == Mode.positional
-        self.memory[args[-1].value] = self._get_arg(args[0]) * self._get_arg(args[1])
+        assert args[-1].mode != Mode.immediate
+        self._set_dst(args[-1], self._get_arg(args[0]) * self._get_arg(args[1]))
         return self.ip + len(args) + 1
 
     def inp(self, args: list[Argument]) -> int:
-        assert args[-1].mode == Mode.positional
+        assert args[-1].mode != Mode.immediate
         if self.inputs:
-            self.memory[args[-1].value] = self.inputs.pop(0)
+            self._set_dst(args[-1], self.inputs.pop(0))
             return self.ip + len(args) + 1
         else:
             self.state = State.paused
@@ -80,13 +114,17 @@ class Computer:
             return self.ip + len(args) + 1
 
     def les(self, args: list[Argument]) -> int:
-        assert args[-1].mode == Mode.positional
-        self.memory[args[-1].value] = int(self._get_arg(args[0]) < self._get_arg(args[1]))
+        assert args[-1].mode != Mode.immediate
+        self._set_dst(args[-1], int(self._get_arg(args[0]) < self._get_arg(args[1])))
         return self.ip + len(args) + 1
 
     def equ(self, args: list[Argument]) -> int:
-        assert args[-1].mode == Mode.positional
-        self.memory[args[-1].value] = int(self._get_arg(args[0]) == self._get_arg(args[1]))
+        assert args[-1].mode != Mode.immediate
+        self._set_dst(args[-1], int(self._get_arg(args[0]) == self._get_arg(args[1])))
+        return self.ip + len(args) + 1
+
+    def rbo(self, args: list[Argument]) -> int:
+        self.rel_base += self._get_arg(args[0])
         return self.ip + len(args) + 1
 
     def hlt(self, args: list[Argument]) -> int:
@@ -102,6 +140,7 @@ class Computer:
         6: {'function': jif, 'args': 2},
         7: {'function': les, 'args': 3},
         8: {'function': equ, 'args': 3},
+        9: {'function': rbo, 'args': 1},
         99: {'function': hlt, 'args': 0},
     }
 
@@ -113,9 +152,8 @@ class Computer:
 
         while self.state == State.running:
             # read next instruction from memory and convert it to zero padded string
-            instruction = f'{self.memory[self.ip]:05}'
+            instruction = f'{self[self.ip]:05}'
             opcode = int(instruction[-2:])  # extract opcode
-            # modes = [int(val) for val in reversed(instruction[:3])]  # extract modes
 
             # get instruction details
             instruction_dict = self.INSTRUCTION_SET[opcode]
@@ -124,7 +162,7 @@ class Computer:
             args = [
                 Argument(Mode(mode), val)
                 for val, mode in zip(
-                    self.memory[self.ip + 1:self.ip + 1 + num_args],
+                    self[self.ip + 1:self.ip + 1 + num_args],
                     [int(val) for val in reversed(instruction[:3])]
                 )
             ]
@@ -138,11 +176,19 @@ class Computer:
 if __name__ == "__main__":
     # # mem[3] = mem[0] + mem[0]; halt
     # test = Computer([1, 0, 0, 3, 99])
+
     # # mem[3] = 1 + 0; halt
     # test = Computer([1101, 1, 0, 3, 99])
+
     # # mem[3] = mem[9] + mem[10]; mem[0] = mem[3] * mem[11]; halt
     # test = Computer([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])
-    # mem[0] = input[0]; mem[0] = mem[0] * mem[8]; halt
-    test = Computer([3, 0, 2, 0, 8, 0, 4, 0, 99], [5])
 
-    test.run()
+    # # mem[0] = input[0]; mem[0] = mem[0] * mem[8]; output[0]; halt
+    # test = Computer([3, 0, 2, 0, 8, 0, 4, 0, 99], [5])
+
+    # mem[0] = input[0]; mem[20] = mem[20] * mem[8]; output[20]; halt :: len(mem) < 20
+    test = Computer([3, 0, 2, 0, 8, 20, 4, 20, 99], [5])
+
+    test.dump()
+
+    # test.run()
